227de9dc8f79c639962bccbed7078907ae41e1a4
st1064870 <st1064870@ceid.upatras.gr>
2020-05-06 17:46:36 +0300

    Minor changes to code

--- examples/hello-world/hello-world.c
+++ examples/hello-world/hello-world.c
@@ -38,7 +38,7 @@
  */
 
 #include "contiki.h"
-
+#include "dev/leds.h"
 #include <stdio.h> /* For printf() */
 /*---------------------------------------------------------------------------*/
 PROCESS(hello_world_process, "Hello world process");
@@ -47,21 +47,17 @@ AUTOSTART_PROCESSES(&hello_world_process);
 PROCESS_THREAD(hello_world_process, ev, data)
 {
   static struct etimer timer;
-
+  static char counter;
   PROCESS_BEGIN();
 
-  /* Setup a periodic timer that expires after 10 seconds. */
-  etimer_set(&timer, CLOCK_SECOND);
-
-  int counter = 0;
-  leds_init();
+  counter = 0;
   while (1) {
+    etimer_set(&timer, CLOCK_SECOND);
     PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer));
     leds_set(counter);
+    printf("%x %x\n", counter, leds_get());
     counter++;
     if (counter == 8) { counter = 0; }
-    /* Wait for the periodic timer to expire and then restart the timer. */
-    etimer_reset(&timer);
   }
 
   PROCESS_END();
